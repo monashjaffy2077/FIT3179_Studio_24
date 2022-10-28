2626cb0572cfeb3d23b4a855f7399f6ff91341fc
dcuu0001 <dcuu0001@student.monash.edu>
2022-10-28 22:33:16 +1100

    main typo link

--- Data Visualization 2/js/vg_script.js
+++ Data Visualization 2/js/vg_script.js
@@ -1,14 +1,14 @@
-var vg_1 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/Data%20Visualization%202/js/vacc_map.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/main/Data%20Visualization%202/js/vacc_map.vg.json";
 vegaEmbed("#vacc_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/%20Visualization%202/js/vacc_cases.vg.json";
+var vg_2 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/main/%20Visualization%202/js/vacc_cases.vg.json";
 vegaEmbed("#dot_plot", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/Data%20Visualization%202/js/vacc_brand.vg.json";
+var vg_3 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/main/Data%20Visualization%202/js/vacc_brand.vg.json";
 vegaEmbed("#pie_plot", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);

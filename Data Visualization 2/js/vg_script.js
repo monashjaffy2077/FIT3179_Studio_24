a7f6f1bb47fa751afcbbb3aeaa0435273d83ccc9
dcuu0001 <dcuu0001@student.monash.edu>
2022-10-29 11:43:56 +1100

    concat plot aus

--- Data Visualization 2/js/vg_script.js
+++ Data Visualization 2/js/vg_script.js
@@ -12,3 +12,8 @@ var vg_3 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/
 vegaEmbed("#pie_plot", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_4 = "https://raw.githubusercontent.com/monashjaffy2077/FIT3179_Studio_24/main/Data%20Visualization%202/js/vacc_aus.vg.json";
+vegaEmbed("#hc_plot", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
